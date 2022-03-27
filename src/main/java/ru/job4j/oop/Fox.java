5e87eaa5784e526257a5c08d492ee176bc7ccd05
USER <tomilin-2000@mail.ru>
2022-03-27 11:39:11 +0300

    1.6. Взаимодействие объектов [#166087 #280568]

--- src/main/java/ru/job4j/oop/Fox.java
+++ src/main/java/ru/job4j/oop/Fox.java
@@ -2,7 +2,6 @@ package ru.job4j.oop;
 
 public class Fox {
     public void tryEat(Ball ball) {
-        Ball ball1 = new Ball();
-        ball1.tryRun(true);
+        ball.tryRun(true);
     }
 }
--- src/main/java/ru/job4j/oop/Wolf.java
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -2,7 +2,6 @@ package ru.job4j.oop;
 
 public class Wolf {
     public void tryEat(Ball ball) {
-        Ball ball1 = new Ball();
-        ball1.tryRun(false);
+        ball.tryRun(false);
     }
 }
