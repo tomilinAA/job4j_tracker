7f88b01b5bd4210564d094afea4f60d1e6d7745f
USER <tomilin-2000@mail.ru>
2022-04-06 21:45:57 +0300

    3. Перегрузить метод max для трех чисел [#122644 #284981]

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -6,16 +6,12 @@ public class Max {
 
     }
 
-    public static int summation(int first, int second) {
-        return first + second;
-    }
-
     public static int summation(int first, int second, int third) {
-        return summation(first, summation(second, third));
+        return max(first, max(second, third));
     }
 
     public static int summation(int first, int second, int third, int fourth) {
-        return summation(first, summation(second, third, fourth));
+        return max(first, summation(second, third, fourth));
     }
 
     public static void main(String[] args) {
