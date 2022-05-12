0e53a7b345c901b088b0cef6ef551421b42c3221
USER <tomilin-2000@mail.ru>
2022-05-12 15:45:00 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+public class Bus implements Transport {
+
+    @Override
+    public void goStraight() {
+
+    }
+
+    @Override
+    public void passengers(int pass) {
+
+    }
+
+    @Override
+    public int price(int fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+public interface Transport {
+
+    void goStraight();
+
+    void passengers(int pass);
+
+    int price(int fuel);
+}
