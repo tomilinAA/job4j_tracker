04299f597a707e6eb69c9c1ce9a54c5853710685
USER <tomilin-2000@mail.ru>
2022-05-13 09:42:31 +0300

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748 #300114]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -53,7 +53,7 @@ public class StartUI {
                System.out.print("Enter id: ");
                int id = Integer.parseInt(scanner.nextLine());
                Item item = tracker.findById(id);
-               System.out.println(item);
+               System.out.print(item);
                if (item != null) {
                    System.out.println(item);
                } else {
